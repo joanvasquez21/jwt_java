ee37f401e74c68e073fb307661513d0633e312b7
joanvasquez21 <joanvrivas.21@gmail.com>
2024-10-13 20:05:50 +0200

    feat: roles are assigned and the password is encrypted with bcrypt

--- jwt/src/main/java/com/dev/jwt/jwt/services/UserServiceImpl.java
+++ jwt/src/main/java/com/dev/jwt/jwt/services/UserServiceImpl.java
@@ -1,12 +1,15 @@
 package com.dev.jwt.jwt.services;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.dev.jwt.jwt.entities.Role;
 import com.dev.jwt.jwt.entities.User;
 import com.dev.jwt.jwt.repositories.RoleRepository;
 import com.dev.jwt.jwt.repositories.UserRepository;
@@ -33,6 +36,22 @@ public class UserServiceImpl implements UserService{
     @Override
     @Transactional
     public User save(User user) {
+
+        Optional<Role> optionalRoleUser = roleRepository.findByName("ROLE_USER");
+
+        List<Role> roles = new ArrayList<>();
+
+        optionalRoleUser.ifPresent(role -> roles.add(role));
+
+        if(user.isAdmin()){
+            Optional<Role> optionalRoleAdmin = roleRepository.findByName("ROLE_ADMIN");
+            optionalRoleAdmin.ifPresent(role -> roles.add(role));
+        }
+
+        user.setRoles(roles);
+        String encodedPassword = passwordEncoder.encode(user.getPassword());
+        user.setPassword(encodedPassword);
+
         return userRepository.save(user);
     }
 }
