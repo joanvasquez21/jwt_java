a577cfd4ceb73f35428a8b29f4781abe77b3b8f9
joanvasquez21 <joanvrivas.21@gmail.com>
2024-11-09 15:14:05 +0100

    feat: roles are added to claims

--- jwt/src/main/java/com/dev/jwt/jwt/security/filter/JwtAuthenticationFilter.java
+++ jwt/src/main/java/com/dev/jwt/jwt/security/filter/JwtAuthenticationFilter.java
@@ -1,15 +1,18 @@
 package com.dev.jwt.jwt.security.filter;
 
+import static com.dev.jwt.jwt.security.TokenJwtConfig.*;
+
 import java.io.IOException;
+import java.util.Collection;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
-
-import javax.crypto.SecretKey;
 
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
@@ -17,78 +20,79 @@ import com.fasterxml.jackson.core.exc.StreamReadException;
 import com.fasterxml.jackson.databind.DatabindException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-
 public class JwtAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
 
     //Para decir que usuarios son autetenticos
     private AuthenticationManager authenticationManager;
- 
-    private static final SecretKey SECRET_KEY= Jwts.SIG.HS256.key().build();
-
-    
 
     public JwtAuthenticationFilter(AuthenticationManager authenticationManager) {
         this.authenticationManager = authenticationManager;
     }
 
-
     @Override
     public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response)
             throws AuthenticationException {
-                //mediante el request obtenemos el username y password
-                //1.- tomar el jsn y lo convertimos a un objeto de java
-                User user = null;
-                String username = null;
-                String password = null;
+        //mediante el request obtenemos el username y password
+        //1.- tomar el jsn y lo convertimos a un objeto de java
+        User user = null;
+        String username = null;
+        String password = null;
 
-                try {
-                    user = new ObjectMapper().readValue(request.getInputStream(), User.class);
-                    username = user.getUsername();
-                    password = user.getPassword();
-                } catch (StreamReadException e) {
-                    e.printStackTrace();
-                } catch (DatabindException e) {
-                    e.printStackTrace();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-                //se crea un username.... con el nombre del username y el password
-                UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(username, password);
-                //llama a authenticate para verificar las credenciales
-                return authenticationManager.authenticate(authenticationToken);
+        try {
+            user = new ObjectMapper().readValue(request.getInputStream(), User.class);
+            username = user.getUsername();
+            password = user.getPassword();
+        } catch (StreamReadException e) {
+            e.printStackTrace();
+        } catch (DatabindException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        //se crea un username.... con el nombre del username y el password
+        UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(username, password);
+        //llama a authenticate para verificar las credenciales
+        return authenticationManager.authenticate(authenticationToken);
     }
 
-
-
     @Override
-    protected void successfulAuthentication(HttpServletRequest request, 
-                                            HttpServletResponse response, 
-                                            FilterChain chain,
-                                            Authentication authResult) throws IOException, ServletException {
+    protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain,
+            Authentication authResult) throws IOException, ServletException {
 
-        User user =  (User) authResult.getPrincipal();
+        //Obtenemos el username
+        User user = (User) authResult.getPrincipal();
         String username = user.getUsername();
 
-        String jwt = Jwts.builder()
-                         .subject(username)
-                         .signWith(SECRET_KEY)
-                         .compact();
-        //devolvemos el token a la vista
-        response.addHeader("Authorization", "Bearer " + jwt);
-        //ademas pasamos como respuesta json
-        Map<String, String> body = new HashMap<>();
+        Collection<? extends GrantedAuthority> roles = authResult.getAuthorities();
+        Claims claims = Jwts.claims().build();
+        claims.put("authorities", roles);
+        
 
-        body.put("token", jwt);
+
+        String token = Jwts.builder()
+                .subject(username)
+                .claims(claims)
+                .expiration(new Date(System.currentTimeMillis() + 3600000))
+                .issuedAt(new Date())
+                
+                .signWith(SECRET_KEY)
+                .compact();
+        response.addHeader(HEADER_AUTHORIZATION, PREFIX_TOKEN + token);
+
+        Map<String, String> body = new HashMap<>();
+        body.put("token", token);
         body.put("username", username);
-        body.put("message", String.format("Hello %s you have logged in successfully!", username));
-        //Escribimos este json a la respuesta
+        body.put("message", String.format("Has %s has iniciado sesion con exito", username));
+
         response.getWriter().write(new ObjectMapper().writeValueAsString(body));
         response.setContentType("application/json");
         response.setStatus(200);
     }
+
 }
